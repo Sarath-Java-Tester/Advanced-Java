7fae4a7312d671d809dd82fdbfc4df1f1de66ac3
Sarath-Java-Tester <sarath.java.tester@gmail.com>
2023-06-17 19:38:56 +0530

    MainDriver1 Java Source File Uploaded

---
+++ MainDriver1.java
@@ -0,0 +1,15 @@
+class MainDriver1 {
+	public static void main(String[] args) {
+		ArithmeticOperation.addition(12,24);
+		System.out.println();
+		ArithmeticOperation.subtraction(12,24);
+		System.out.println();
+		ArithmeticOperation.multiplication(12,24);
+		System.out.println();
+		ArithmeticOperation.quotientAfterDivision(12,24);
+		System.out.println();
+		ArithmeticOperation.remainderAfterDivision(12,24);
+		System.out.println();
+
+	}
+}
